667d76ac868a734e719ed7c3475392f383231d95
Nicole Dagnenica <nnardella@gmail.com>
2020-04-01 12:51:39 -0400

    Add routes for login, signup and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post '/api/v1/login' => 'api/v1/sessions#create'
+  post 'api/v1/signup' => 'api/v1/users#create'
+  delete 'api/v1/logout' => 'api/v1/users#destroy'
+  get 'api/v1/get_current_user' => 'api/v1/sessions#get_current_user'
+
 end
+
+
