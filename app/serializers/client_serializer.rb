92c40edeadb9fb72e9f011a5ea5063f6442a60a5
Nicole Dagnenica <nnardella@gmail.com>
2020-04-04 15:14:01 -0400

    Add project relationship data to client serializer

--- app/serializers/client_serializer.rb
+++ app/serializers/client_serializer.rb
@@ -3,6 +3,19 @@ class ClientSerializer
     
     attributes :name
 
+
+    attribute :projects do |client|
+      client.projects.map do |project|
+        {
+          name: project.name,
+          department: project.department,
+          desc: project.desc,
+          user_id: project.user_id,
+          target_completion_date: project.target_completion_date,
+          completion_date: project.completion_date,
+        }
+      end
+    end
     # has_many :projects, dependent: :destroy, serializer:ProjectSerializer
     # has_many :users, through: :projects, serializer:UserSerializer
 
