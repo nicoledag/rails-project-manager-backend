240bb8cd4478d96b8e0bd52e3458378806034b17
Nicole Dagnenica <nnardella@gmail.com>
2020-04-04 12:54:39 -0400

    Add project associations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,8 @@
 class Project < ApplicationRecord
+
+    belongs_to :user
+    belongs_to :client
+    has_many :comments, dependent: :destroy
+
+    validates :name, :department, :client, :target_completion_date, presence: true
 end
