551e38f6d6989a371a31ede4dc099cb81923befb
Nicole Dagnenica <nnardella@gmail.com>
2020-04-04 14:01:40 -0400

    Add index action to serial project data to json format

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,2 +1,9 @@
 class API::V1::ProjectsController < ApplicationController
+
+    def index
+        @projects = Project.all 
+        projects_json = ProjectSerializer.new(@projects).serialized_json
+        render json: projects_json
+    end
+
 end
