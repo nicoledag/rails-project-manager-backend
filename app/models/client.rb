3fd8695fff10a60a144be65b606544dc0d001745
Nicole Dagnenica <nnardella@gmail.com>
2020-04-04 12:56:30 -0400

    Add client associations

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,10 @@
 class Client < ApplicationRecord
+   
+    has_many :projects, dependent: :destroy
+    has_many :users, through: :projects
+  
+    validates :name, presence: true
+    validates_uniqueness_of :name, :case_sensitive => false
+    # validates :zip, length: { minimum: 5 }
+    
 end
