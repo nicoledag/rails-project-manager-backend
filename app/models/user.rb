cb62e830a3f5eb3ee8a3d3962917a996a7152fe3
Nicole Dagnenica <nnardella@gmail.com>
2020-04-04 12:49:27 -0400

    Add user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
+
+    has_many :projects
+    has_many :clients, through: :projects
     
     validates :username, :email, presence: true
     validates :username, :email, uniqueness: true
