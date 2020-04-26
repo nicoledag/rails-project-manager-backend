58b9963c2f5769de9835800fb0e7c6b3081c52c7
Nicole Dagnenica <nnardella@gmail.com>
2020-04-26 07:20:17 -0400

    Create methods create, update and destroy in clients controller

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -5,4 +5,51 @@ class API::V1::ClientsController < ApplicationController
         clients_json = ClientSerializer.new(@clients).serialized_json
         render json: clients_json
     end
+
+    def create
+        if current_user.admin
+          @client = Client.new(client_params)
+          if @client.save
+            render json: ClientSerializer.new(@client).serialized_json, status: :created
+          else
+            response = {
+                error: @client.errors_full_messages.to_sentence
+            }
+            render json: response, status: :unprocessable_entity
+          end
+        end
+      end
+
+      def update
+        if current_user.admin && @client.update(client_params)
+            render json: ClientSerializer.new(@client).serialized_json, status: :created
+        else
+            response = {
+                error: @client.errors_full_messages.to_sentence
+            }
+            render json: response, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        if current_user.admin
+          @client.destroy
+          render json: ClientSerializer.new(@client).serialized_json, status: :created
+        else
+            render json: {error: 'Cannot destroy'}
+
+        end
+      end
+    
+      private
+
+      def set_client
+        @client = Client.find_by(id: params[:id])
+      end
+    
+      def client_params
+        params.require(:client).permit(:name)
+      end
+    
+    
 end
