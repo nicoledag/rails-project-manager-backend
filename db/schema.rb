3b03b882be4647acb601324dff32229194bfd768
Nicole Dagnenica <nnardella@gmail.com>
2020-04-26 07:22:25 -0400

    Add admin to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_110518) do
+ActiveRecord::Schema.define(version: 2020_04_26_112129) do
 
   create_table "clients", force: :cascade do |t|
     t.string "name"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_04_26_110518) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
   end
 
 end
